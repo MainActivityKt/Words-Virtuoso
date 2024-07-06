7f4058fa6a5994016f369dd89f594b1aa7fc2cb0
Safi <safiullahkakar2020@outlook.com>
2024-07-07 00:36:47 +0430

    Solved stage1

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,18 @@
+fun validateInput(input: String) {
+    input.apply {
+        if (length != 5) {
+            println("The input isn't a 5-letter string.")
+        } else if(toSet().size != length) {
+            println("The input has duplicate letters.")
+        } else if(contains(Regex("[^A-Za-z]"))) {
+            println("The input has invalid characters.")
+        } else {
+            println("The input is a valid string.")
+        }
+    }
+}
+fun main() {
+    println("Input a 5-letter string:")
+    val input = readln()
+    validateInput(input)
+}
