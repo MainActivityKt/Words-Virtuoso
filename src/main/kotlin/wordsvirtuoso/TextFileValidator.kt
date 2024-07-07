6b3b40aeeeb4e84fb6642d04b14e14999657f696
Safi <safiullahkakar2020@outlook.com>
2024-07-07 13:22:28 +0430

    Solved stage2

---
+++ src/main/kotlin/wordsvirtuoso/TextFileValidator.kt
@@ -0,0 +1,46 @@
+package wordsvirtuoso
+
+import java.io.File
+import java.io.FileNotFoundException
+import kotlin.system.exitProcess
+
+class TextFileValidator {
+    private val inputFile: File
+    private var invalidWordsNum: Int
+
+    init {
+        println("Input the words file:")
+        val fileName = readln()
+        inputFile = File(fileName)
+        if (inputFile.exists()) {
+            invalidWordsNum = 0
+        } else {
+            println("Error: The words file $fileName doesn't exist.")
+            exitProcess(0)
+        }
+    }
+
+    fun countValidWords() {
+        inputFile.readLines().forEach {
+            if (!isValidWord(it)) {
+                invalidWordsNum++
+            }
+        }
+    }
+
+    fun printValidWords() {
+        println(if (invalidWordsNum == 0) "All words are valid!" else "Warning: $invalidWordsNum invalid words were found in the ${inputFile.name} file.")
+    }
+
+    private fun isValidWord(word: String): Boolean {
+        word.apply {
+            return length == 5 && toSet().size == length && !contains(Regex("[^A-Za-z]"))
+        }
+    }
+}
+
+fun main() {
+    val validator = TextFileValidator()
+    validator.countValidWords()
+    validator.printValidWords()
+}
